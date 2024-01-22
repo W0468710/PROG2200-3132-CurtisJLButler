0e14b9fe689cab8ce97c1bd13beafd01a9a7206f
Curtis Butler <curtisjlbutler@gmail.com>
2024-01-22 14:45:42 -0400

    Program 3 finished

---
+++ Lab1/Module1/Section3/ForLoopCounting/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ForLoopCounting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 1; x <= 5; x++) {
+                for (int i = 1; i < 11; i++)
+                {
+                    Console.WriteLine(i);
+                }
+
+                for (int i = 10; i > 0; i--)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+           
+        }
+    }
+}
