96375060a403eb38ec1fe3db00f6db5f2b424bdf
Curtis Butler <curtisjlbutler@gmail.com>
2024-01-22 13:55:59 -0400

    Program 1 complete

---
+++ Lab1/Module1/Section1/MultipleChoiceQuestions/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace MultipleChoiceQuestions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("What is your age?");
+            var age = Console.ReadLine();
+
+            Console.WriteLine("What is your birth month?");
+            var month = Console.ReadLine();
+
+            // Prints a blank line to make the console look nicer and more readable
+            Console.WriteLine("");
+            Console.WriteLine("Your name is: {0}", name);
+
+            Console.WriteLine("Your age is: {0}", age);
+
+            Console.WriteLine("Your birth month is: {0}", month);
+
+
+
+        }
+    }
+}
