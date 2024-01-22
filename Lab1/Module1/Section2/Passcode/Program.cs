7e7470fbe0f7a62eeab67a42cc2166e99ca378e0
Curtis Butler <curtisjlbutler@gmail.com>
2024-01-22 14:21:56 -0400

    program 2 finnished

---
+++ Lab1/Module1/Section2/Passcode/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Passcode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the passcode?");
+            var code = Console.ReadLine();
+
+            if (code == "secret")
+            {
+                Console.WriteLine("Authenticated");
+            }
+            else if(code != "secret")
+            {
+                Console.WriteLine("Not Authenticated");
+            }
+        }
+    }
+}
