ae7e729465a69323aa7cc229159525b50364761d
Curtis Butler <curtisjlbutler@gmail.com>
2024-01-22 14:01:46 -0400

    Question one of lab 1 finished

--- Lab1/Module1/Section1/MultipleChoiceQuestions/Program.cs
+++ Lab1/Module1/Section1/MultipleChoiceQuestions/Program.cs
@@ -4,8 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your name?");
+            Console.WriteLine("What is your first name?");
             var name = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            var lastname = Console.ReadLine();
 
             Console.WriteLine("What is your age?");
             var age = Console.ReadLine();
@@ -13,13 +16,19 @@
             Console.WriteLine("What is your birth month?");
             var month = Console.ReadLine();
 
+            Console.WriteLine("What is your birth day?");
+            var day = Console.ReadLine();
+
             // Prints a blank line to make the console look nicer and more readable
             Console.WriteLine("");
-            Console.WriteLine("Your name is: {0}", name);
+
+            Console.WriteLine("Your full name is: {0} {1}", name, lastname);
 
             Console.WriteLine("Your age is: {0}", age);
 
             Console.WriteLine("Your birth month is: {0}", month);
+
+            Console.WriteLine("Your birth day is: {0}", day);
 
 
 
