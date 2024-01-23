f8fb799ef3565c26a9b890faa227c7342d4b27cd
Curtis Butler <curtisjlbutler@gmail.com>
2024-01-23 19:37:32 -0400

    Program 4 fixed

---
+++ Lab1/Module1/Section4/Student Manager/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Student_Manager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many students in your class?");
+            var studentCount = int.Parse(Console.ReadLine());
+
+            var studentNames = new string[studentCount];
+            var studentGrades = new int[studentCount];
+
+            for (int i = 0; i < studentCount; i++)
+            {
+                Console.WriteLine("Student Name: ");
+                studentNames[i] = Console.ReadLine();
+
+                Console.WriteLine("Student Grade: "); // Change to "Student Grade" instead of "Student Number"
+                studentGrades[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < studentCount; i++)
+            {
+                Console.WriteLine("Name: {0}, Grade: {1}", studentNames[i], studentGrades[i]);
+            }
+        }
+    }
+}
