dd2aaaed76d164c0a551cca4086dce1c1cca669a
Curtis Butler <curtisjlbutler@gmail.com>
2024-01-23 21:26:08 -0400

    Module 2 section 1 completed

--- Lab1/Module2/Section1/Survey/Program.cs
+++ Lab1/Module2/Section1/Survey/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Xml.Linq;
+﻿using System.Runtime.InteropServices;
+using System.Xml.Linq;
 
 namespace Survey
 {
@@ -6,7 +7,7 @@ namespace Survey
     {
         public string FirstName;
         public string LastName;
-        public string Month;
+        public int Month;
         public int Age;
         public int Day;
 
@@ -20,18 +21,45 @@ namespace Survey
             Console.WriteLine("Your age is: {0}", Age);
             Console.WriteLine("Your birth month is: {0}", Month);
             Console.WriteLine("Your birth day is: {0}", Day);
-
-            if (Month == "march")
+            
+            switch(Month)
             {
-                Console.WriteLine("You are an Aries.");
-            }
-            else if (Month == "april")
-            {
-                Console.WriteLine("You are a Taurus.");
-            }
-            else if (Month == "may")
-            {
-                Console.WriteLine("You are a Gemini");
+                case 1:
+                    Console.WriteLine("You are an Aquarius.");
+                    break;
+                case 2:
+                    Console.WriteLine("You are an Pisces.");
+                    break;
+                case 3:
+                    Console.WriteLine("You are an Aries.");
+                    break;
+                case 4:
+                    Console.WriteLine("You are a Taurus.");
+                    break;
+                case 5:
+                    Console.WriteLine("You are a Gemini");
+                    break;
+                case 6:
+                    Console.WriteLine("You are a Cancer");
+                    break;
+                case 7:
+                    Console.WriteLine("You are a Leo");
+                    break;
+                case 8:
+                    Console.WriteLine("You are a Virgo");
+                    break;
+                case 9:
+                    Console.WriteLine("You are a Libra");
+                    break;
+                case 10:
+                    Console.WriteLine("You are a Scorpio");
+                    break;
+                case 11:
+                    Console.WriteLine("You are a Sagittarius");
+                    break;
+                case 12:
+                    Console.WriteLine("You are a Capricorn");
+                    break;
             }
         }
     }
@@ -39,32 +67,72 @@ namespace Survey
     {
         static void Main(string[] args)
         {
+
             var data = new Data();
             Console.WriteLine("What is your first name?");
-            data.FirstName = TryAnswer();
+            data.FirstName = TryAnswer("name");
 
             Console.WriteLine("What is your last name?");
-            data.LastName = TryAnswer();
+            data.LastName = TryAnswer("name");
 
             Console.WriteLine("What is your age?");
-            data.Age = int.Parse(TryAnswer());
+            data.Age = int.Parse(TryAnswer("age"));
 
             Console.WriteLine("What is your birth month?");
-            data.Month = TryAnswer();
+            data.Month = int.Parse(TryAnswer("month"));
 
             Console.WriteLine("What is your birth day?");
-            data.Day = int.Parse(TryAnswer());
+            data.Day = int.Parse(TryAnswer("day"));
 
             data.Display();
 
-            static string TryAnswer()
+            static string TryAnswer(string entryType)
             {
-                var question = Console.ReadLine();
-                if (question == "")
+                while (true)
                 {
-                    Console.WriteLine("You didn't type anything, please try again.");
+                    var question = Console.ReadLine();
+
+
+                    if (entryType == "month")
+                    {
+                        if ((int.Parse(question) > 12 || int.Parse(question) < 1))
+                        {
+                            Console.WriteLine("That is not a month");
+                        }
+                        else if ((int.Parse(question) < 12 || int.Parse(question) > 1))
+                        {
+                            return question;
+                            break;
+                        }
+                        else if (question == "")
+                        {
+                            Console.WriteLine("You didn't type anything, please try again.");
+                        }
+                    }
+                    else if (entryType == "day")
+                    {
+                        if ((int.Parse(question) > 31 || int.Parse(question) < 1))
+                        {
+                            Console.WriteLine("That is not a day");
+                        }
+                        else if ((int.Parse(question) < 31 || int.Parse(question) > 1))
+                        {
+                            return question;
+                            break;
+                        }
+                    }
+                    else if (question == "")
+                    {
+                        Console.WriteLine("You didn't type anything, please try again.");
+                    }
+                    else
+                    {
+                        return question;
+                        break;
+                    }
+                    
                 }
-                return question; 
+                
 
                 
             }
